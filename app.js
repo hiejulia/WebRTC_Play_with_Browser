33ca46709c3148f18d0b9bf757950efb1fce7acf
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-02-18 00:58:40 +0200

    continue config...
    1:00 am .... want to continue coding but have to go the sleep for better
    health

--- app.js
+++ app.js
@@ -41,14 +41,81 @@ function getWebcam(stream){
 //createPeerConnectionStream
 function createPeerConnectionStream(stream){
   //create local peer connection
+  myRtcPeerConnection = new PeerConnection(null);
+  console.log('Local peer connection object created');
+  //create remote peer connection
+  myRemotePeerConnection = new PeerConnection(null);
+  console.log('Created remote peer connection object');
+  //listen for ICE candidate on each
+  myRemotePeerConnection.onicecandidate = getMyIceCandidate;
+  myRemotePeerConnection.onicecandidate = getMyIceCandidata;
+  //handle streams on each peer
+  myRtcPeerConnection.addStream(stream);
+  console.log('Local stream added to myRtcPeerConnection');
+  myRemotePeerConnection.onaddstream = getRemoteStream;
+  //create local peer connection offer
+  myRemotePeerConnection.createOffer(getLocalDescription);
+  console.log('SDP offer on myRtcPeerConnection is created! :) ');
 
 
+};
 
-
-
-
-
-
-
+//getMyIceCandidata
+function getMyIceCandidata(e){
+  if(e.candidate){
+    //send the local ice candidate to the remote peer
+    myRemotePeerConnection.addIceCandidate(new RTCIceCandidate(e.candidate));
+    confirm.log('sent my ICE candidate to myRemotePeerConnection');
+  }
 
 }
+//when remote ICE candidate is received by me....
+function getRemoteIceCandidate(e){
+  if(e.candidate){
+    //add the remote ice candidate to my local peer connnection
+    myRtcPeerConnection.addIceCandidate(new RTCIceCandidate(e.candidate));
+    console.log('Added remote ICE candidates to myRtcPeerConnection');
+  }
+}
+//create SDP offer
+function getLocalDescription(des){
+  myRtcPeerConnection.setLocalDescription(des);
+  console.log('Offer from myRtcPeerConnection is created');
+  myRemotePeerConnection.setRemoteDescription(des);
+  myRemotePeerConnection.createAnswer(getRemoteDescription);
+
+}
+//when remote SDP arrives
+function getRemoteDescription(des){
+  myRemotePeerConnection.setLocalDescription(des);
+  console.log('Got answer from remotePeerConnection');
+  myRtcPeerConnection.setRemoteDescription(des);
+}
+
+//getRemoteStream.. show the remote video
+function getRemoteStream(e){
+  video2.src = URL.createObjectURL(e.stream);
+  //play video2
+  video2.play();
+  console.log('Got remote stream back');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
